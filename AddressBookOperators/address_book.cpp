a453141426f2fe85119159b8bdb6105023cbcb18
m.ekstrin <m.ekstrin@tinkoff.ru>
2022-12-04 17:18:30 +0300

    hw

---
+++ AddressBookOperators/address_book.cpp
@@ -0,0 +1,73 @@
+//
+// Created by m.ekstrin on 02.12.2022.
+//
+#include <iostream>
+#include <string>
+#include <forward_list>
+#include "employee.hpp"
+#include "address_book.hpp"
+
+namespace AddressBookModule {
+
+    AddressBook &AddressBook::operator=(const AddressBook &another) {
+        this->clear();
+        for (auto& emp: another.address_book){
+            this->add(emp);
+        }
+        return *this;
+    }
+
+    void AddressBook::add(const Employee &emp) {
+        address_book.push_front(emp);
+    }
+
+    void AddressBook::remove_by_id(uint32_t id) {
+        address_book.remove(find_by_id(id));
+    }
+
+    Employee &AddressBook::find_by_id(uint32_t id) {
+        for (auto &emp: address_book) {
+            if (emp.id == id) {
+                return emp;
+            }
+        }
+        address_book.push_front(Employee());
+        return address_book.front();
+    }
+
+    Employee &AddressBook::find_by_name(const std::string &name) {
+        for (auto &emp: address_book) {
+            if (emp.name == name)
+                return emp;
+        }
+        address_book.push_front(Employee());
+        return address_book.front();
+    }
+
+    void AddressBook::clear() {
+        address_book.clear();
+    }
+
+    std::ostream &operator<<(std::ostream &out, const AddressBook &book) {
+        for (auto &emp: book.address_book) {
+            out << emp << std::endl;
+        }
+        return out;
+    }
+
+    void AddressBook::operator+(const Employee &emp) {
+        add(emp);
+    }
+
+    void AddressBook::operator-(Employee &emp) {
+        remove_by_id(emp.id);
+    }
+
+    Employee &AddressBook::operator[](int index) {
+        return find_by_id(index);
+    }
+
+    Employee &AddressBook::operator[](const std::string &index_name) {
+        return find_by_name(index_name);
+    }
+}
---
+++ AddressBookOperators/address_book.hpp
@@ -0,0 +1,44 @@
+//
+// Created by m.ekstrin on 02.12.2022.
+//
+#include <forward_list>
+#include "employee.hpp"
+
+#ifndef HW1_ADDRESS_BOOK_HPP
+#define HW1_ADDRESS_BOOK_HPP
+
+
+namespace AddressBookModule {
+
+    class AddressBook {
+        std::forward_list<Employee> address_book;
+    public:
+        AddressBook() = default;
+
+
+        void add(const Employee &emp);
+
+        void remove_by_id(uint32_t id);
+
+        Employee &find_by_id(uint32_t id);
+
+        Employee &find_by_name(const std::string &name);
+
+        void clear();
+
+        friend std::ostream &operator<<(std::ostream &out, const AddressBook &book);
+
+        void operator+(const Employee &emp);
+
+        void operator-(Employee &emp);
+
+        Employee &operator[](int index);
+
+        Employee &operator[](const std::string &index_name);
+
+
+        AddressBook &operator=(const AddressBook &another);
+
+    };
+};
+#endif //HW1_ADDRESS_BOOK_HPP
