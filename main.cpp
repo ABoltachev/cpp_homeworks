0d9bc721a950a04040add443a882b8b949cdc4a5
George Golubev <george.gol003@gmail.com>
2023-02-07 23:05:59 +0300

    implementation of vector

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+#include <array>
 #include <iostream>
+#include <vector>
 
-int main() {
+#include "containers/array.hpp"
+#include "containers/vector.hpp"
+
+int main()
+{
+    lib::Vector<int> vector(1, 12);
+
+    vector.push_back(1);
+    vector.push_back(1);
+    vector.push_back(1);
+    vector.clear();
+    vector.resize(1);
     std::cout << "Hello, test!" << std::endl;
     return 0;
 }
