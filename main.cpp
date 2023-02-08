b06ae180ab6383d800612f3fcf28baf4ec7427ed
George Golubev <george.gol003@gmail.com>
2023-02-08 13:23:18 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -7,13 +7,6 @@
 
 int main()
 {
-    lib::Vector<int> vector(1, 12);
-
-    vector.push_back(1);
-    vector.push_back(1);
-    vector.push_back(1);
-    vector.clear();
-    vector.resize(1);
     std::cout << "Hello, test!" << std::endl;
     return 0;
 }
