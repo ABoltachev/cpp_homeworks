dc9c94d09b3c07f50a760886eb852f7bca05fc4d
trixTRr <118841105+trixTRr@users.noreply.github.com>
2023-02-15 18:44:46 +0300

    сделал дз

--- stllib/set.hpp
+++ stllib/set.hpp
@@ -1,12 +1,49 @@
-/*
-* set и методы для него (для реализации использовать простое бинарное дерево, соответственно использовать можно только с данными, которые имеют оператор <):
-    size (возвращает размер)
-    empty (возвращает true если пустой)
-    top (возвращает большее значение)
-    find (поиск элемента, true - нашли, false - нет)
-    insert (вставка (копирование))
-    emplace (вставка (перемещение))
-    clear (очистка
-    присваивание (копирование и перемещением)
-    конструкторы (конструктор без аргументов; копирования; перемещения; конструктор с std::inizialized_list)
-*/
+#pragma once
+
+#include "BinaryTree.h"
+#include <initializer_list>
+
+template<typename T>
+class set {
+	BinaryTree<T> tree_;
+public:
+	set() {}
+		set(set<T> const& other) :tree_(other.tree_) {}
+		set(set<T>&& other) :tree_(std::move(other.tree_)) {}
+	set(std::initializer_list<T> list) {
+		for (auto& i : list)
+			insert(i);
+	}
+	~set() {}
+
+	set<T>& operator=(set<T> const& other) {
+		tree_ = other.tree_;
+		return *this;
+	}
+	set<T>& operator=(set<T>&& other) {
+		tree_ = std::move(other.tree_);
+		return *this;
+	}
+
+	size_t size()const noexcept {
+		return tree_.size();
+	}
+	bool empty()const noexcept {
+		return size() == 0;
+	}
+	const T& top()const noexcept {
+		return tree_.maxRightNodePtr()->getValue();
+	}
+	bool find(T const& value)const noexcept {
+		return tree_.find(value) != nullptr;
+	}
+	void insert(T const& value) {
+		tree_.insert(value);
+	}
+	void emplace(T&& value) {
+		tree_.emplace(std::move(value));
+	}
+	void clear()noexcept {
+		tree_.clear();
+	}
+};
